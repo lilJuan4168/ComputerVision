59691fa8f646481b2ef085eb8f68544c8b4bdade
lilJuan4168 <chesteraraoz12.0@gmail.com>
2024-04-16 00:47:41 -0300

    module 9

--- 05_Capture.py
+++ 05_Capture.py
@@ -1,6 +1,6 @@
 import cv2
 import sys
-
+from time import sleep
 s = 0
 if len(sys.argv) > 1:
     s = sys.argv[1]
@@ -9,7 +9,7 @@ source = cv2.VideoCapture(s)
 
 win_name = 'Camera Preview'
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-
+sleep(5)
 while cv2.waitKey(1) != 27: # Escape
     has_frame, frame = source.read()
     if not has_frame:
